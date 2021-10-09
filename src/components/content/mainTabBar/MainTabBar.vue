64b70996a55eba6d7e72b2f51ebaed214cc37fed
NameZyr <819004045@qq.com>
2021-10-10 03:55:38 +0800

    打包

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <tab-bar class="tab-bar">
+      <tab-bar-item path="/home" activeColor="deepPink">
+         <img slot="item-icon" src="~assets/img/tabbar/1.png" alt="">
+         <img slot="item-icon-active" src="~assets/img/tabbar/1active.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/2.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/2active.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/3.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/3active.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/4.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/4active.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from "components/common/tabbar/TabBar"
+import TabBarItem from "components/common/tabbar/TabBarItem"
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+  .tab-bar {
+    z-index: 999;
+  }
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper>
+    <swiperItem v-for="item in banners">
+      <a href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiperItem>
+  </swiper>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from "components/common/swiper/swiperindex"
+
+export default {
+  name: "HomeSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style>
+
+</style>
